ab3a47e17a7f2bd704ba96296e0e81c79757710e
fassial <2508440340@qq.com>
2020-07-10 00:00:44 +0800

    src: modify cam_cell in cam

--- src/cam/tcam.sv
+++ src/cam/tcam.sv
@@ -18,13 +18,11 @@ module tcam #(
 
     // output index & data
     output  logic                       index_rdy,
-    output  logic[TCAM_INDEX_WIDTH-1:0] index_o,
-    output  logic[TCAM_WIDTH-1:0]       data_o
+    output  logic[TCAM_INDEX_WIDTH-1:0] index_o
 );
 
 // def interface for tcam_cell
 logic[TCAM_DEPTH-1:0] data_match, cell_data_we;
-logic[TCAM_WIDTH-1:0] cell_data_o[TCAM_DEPTH-1:0];
 // def interface for prior_mux
 logic[TCAM_DEPTH-1:0] match_line;
 logic[TCAM_INDEX_WIDTH-1:0] match_index;
@@ -48,12 +46,12 @@ for (genvar i = 0; i < TCAM_DEPTH; ++i) begin : gen_tcam_cell
 
         // input data
         .data_we    ( cell_data_we[i]   ),
+        .data_vld,
         .data_i,
         .data_mask,
 
         // output match
-        .data_match ( data_match[i]     ),
-        .data_o     ( cell_data_o[i]    )
+        .data_match ( data_match[i]     )
     );
 end
 
@@ -73,43 +71,51 @@ assign data_o    = cell_data_o[data_idx];
 endmodule
 
 module tcam_cell #(
-    parameter       TCAM_WIDTH  =   32
+    parameter       TCAM_WIDTH  =   32,
+    localparam type tcam_t      = logic[TCAM_WIDTH-1:0]
 ) (
     // external signals
     input   logic   clk,
     input   logic   rst,
 
     // input data
-    input   logic                   data_we,
-    input   logic[TCAM_WIDTH-1:0]   data_i,
-    input   logic[TCAM_WIDTH-1:0]   data_mask,
+    input   logic   data_we,
+    input   logic   data_vld,
+    input   tcam_t  data_i,
+    input   tcam_t  data_mask,
 
-    // output match & cell_data
-    output  logic                   data_match,
-    output  logic[TCAM_WIDTH-1:0]   data_o
+    // output match
+    output  logic   data_match
 );
 
 // cell_data
-logic[TCAM_WIDTH-1:0] cell_data, cell_data_n;
+tcam_t cell_data, cell_data_n;
+// cell_data_mask
+tcam_t cell_data_mask, cell_data_mask_n;
+// cell_data_vld
+logic cell_data_vld, cell_data_vld_n;
 
 // assign output
 // compare data_i & cell_data
 // if all 0, data_i & cell_data match
-assign data_match = ~|((cell_data ^ data_i) & data_mask);
-// set data_o
-assign data_o     = cell_data;
+assign data_match = (~|((cell_data ^ data_i) & cell_data_mask)) && cell_data_vld;
 
-// update cell_data_n
-always_comb begin
-    // default
-    cell_data_n = cell_data;
-    if (data_we) cell_data_n = data_i;
-end
+// update cell_data_n & cell_data_mask_n & cell_data_vld_n
+assign cell_data_n      = data_i;
+assign cell_data_mask_n = data_mask;
+assign cell_data_vld_n  = data_vld;
 
 // update cell_data
 always_ff @ (posedge clk) begin
-    if (rst) cell_data <= '0;
-    else     cell_data <= cell_data_n;
+    if (rst) begin
+        cell_data       <= '0;
+        cell_data_mask  <= '0;
+        cell_data_vld   <= 1'b0;
+    end else if (data_we) begin
+        cell_data       <= cell_data_n;
+        cell_data_mask  <= cell_data_mask_n;
+        cell_data_vld   <= cell_data_vld_n;
+    end
 end
 
 endmodule
