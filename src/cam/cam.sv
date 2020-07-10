f7d4d3c6028bcb5eb069bb5afbca77d1191f5b52
fassial <2508440340@qq.com>
2020-07-10 12:56:42 +0800

    src: modify tcam & support cam in cam

--- src/cam/cam.sv
+++ src/cam/cam.sv
@@ -1,11 +1,11 @@
-// tcam
+// cam
 `include "cam_defs.svh"
 
-module tcam #(
-    parameter       TCAM_WIDTH  =   32,
-    parameter       TCAM_DEPTH  =   16,
-    localparam      TCAM_INDEX_WIDTH    =   $clog2(TCAM_DEPTH),
-    localparam type tcam_t      = logic[TCAM_WIDTH-1:0]
+module cam #(
+    parameter       CAM_WIDTH   =   32,
+    parameter       CAM_DEPTH   =   16,
+    localparam      CAM_INDEX_WIDTH     =   $clog2(CAM_DEPTH),
+    localparam type cam_t       = logic[CAM_WIDTH-1:0]
 ) (
     // external signals
     input   logic   clk,
@@ -13,21 +13,20 @@ module tcam #(
 
     // input data
     input   logic                       data_we,
-    input   logic[TCAM_INDEX_WIDTH-1:0] data_idx,
-    input   tcam_t                      data_i,
-    input   tcam_t                      data_mask,
+    input   logic[CAM_INDEX_WIDTH-1:0]  data_idx,
+    input   cam_t                       data_i,
     input   logic                       data_vld,
 
     // output index & data
     output  logic                       index_rdy,
-    output  logic[TCAM_INDEX_WIDTH-1:0] index_o
+    output  logic[CAM_INDEX_WIDTH-1:0]  index_o
 );
 
-// def interface for tcam_cell
-logic[TCAM_DEPTH-1:0] data_match, cell_data_we;
+// def interface for cam_cell
+logic[CAM_DEPTH-1:0] data_match, cell_data_we;
 // def interface for prior_mux
-logic[TCAM_DEPTH-1:0] match_line;
-logic[TCAM_INDEX_WIDTH-1:0] match_index;
+logic[CAM_DEPTH-1:0] match_line;
+logic[CAM_INDEX_WIDTH-1:0] match_index;
 
 // set cell_data_we
 always_comb begin
@@ -37,11 +36,11 @@ always_comb begin
     cell_data_we[data_idx] = data_we;
 end
 
-// inst tcam_cell
-for (genvar i = 0; i < TCAM_DEPTH; ++i) begin : gen_tcam_cell
-    tcam_cell #(
-        .TCAM_WIDTH ( TCAM_WIDTH    )
-    ) tcam_cell_inst (
+// inst cam_cell
+for (genvar i = 0; i < CAM_DEPTH; ++i) begin : gen_cam_cell
+    cam_cell #(
+        .CAM_WIDTH  ( CAM_WIDTH )
+    ) cam_cell_inst (
         // external signals
         .clk,
         .rst,
@@ -50,7 +49,6 @@ for (genvar i = 0; i < TCAM_DEPTH; ++i) begin : gen_tcam_cell
         .data_we    ( cell_data_we[i]   ),
         .data_vld,
         .data_i,
-        .data_mask,
 
         // output match
         .data_match ( data_match[i]     )
@@ -59,7 +57,7 @@ end
 
 // inst prior_mux
 prior_mux #(
-    .MUX_WIDTH  ( TCAM_DEPTH    )
+    .MUX_WIDTH  ( CAM_DEPTH )
 ) prior_mux_inst (
     // input match_line
     .match_line,
@@ -75,9 +73,9 @@ assign index_rdy = |data_match;
 
 endmodule
 
-module tcam_cell #(
-    parameter       TCAM_WIDTH  =   32,
-    localparam type tcam_t      = logic[TCAM_WIDTH-1:0]
+module cam_cell #(
+    parameter       CAM_WIDTH   =   32,
+    localparam type cam_t       = logic[CAM_WIDTH-1:0]
 ) (
     // external signals
     input   logic   clk,
@@ -86,39 +84,33 @@ module tcam_cell #(
     // input data
     input   logic   data_we,
     input   logic   data_vld,
-    input   tcam_t  data_i,
-    input   tcam_t  data_mask,
+    input   cam_t   data_i,
 
     // output match
     output  logic   data_match
 );
 
 // cell_data
-tcam_t cell_data, cell_data_n;
-// cell_data_mask
-tcam_t cell_data_mask, cell_data_mask_n;
+cam_t cell_data, cell_data_n;
 // cell_data_vld
 logic cell_data_vld, cell_data_vld_n;
 
 // assign output
 // compare data_i & cell_data
 // if all 0, data_i & cell_data match
-assign data_match = (~|((cell_data ^ data_i) & cell_data_mask)) && cell_data_vld;
+assign data_match = (~|(cell_data ^ data_i)) && cell_data_vld;
 
-// update cell_data_n & cell_data_mask_n & cell_data_vld_n
+// update cell_data_n & cell_data_vld_n
 assign cell_data_n      = data_i;
-assign cell_data_mask_n = data_mask;
 assign cell_data_vld_n  = data_vld;
 
 // update cell_data
 always_ff @ (posedge clk) begin
     if (rst) begin
         cell_data       <= '0;
-        cell_data_mask  <= '0;
         cell_data_vld   <= 1'b0;
     end else if (data_we) begin
         cell_data       <= cell_data_n;
-        cell_data_mask  <= cell_data_mask_n;
         cell_data_vld   <= cell_data_vld_n;
     end
 end
