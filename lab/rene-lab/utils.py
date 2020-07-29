6ecdf7b28aca8f3d79da84a8f97376d8db19402a
fassial <2508440340@qq.com>
2020-07-29 11:27:57 +0800

    lab: add rene in rene-lab

--- lab/rene-lab/utils.py
+++ lab/rene-lab/utils.py
@@ -6,10 +6,14 @@ Created on July 29 10:52, 2020
 import os
 import numpy as np
 
+# file loc params
 PREFIX = "."
 DATASET = os.path.join(PREFIX, "dataset")
 FEATURE_FILE = os.path.join(DATASET, "feature.csv")
 LABEL_FILE   = os.path.join(DATASET, "label.csv")
+# ecode params
+W = 8
+HMAX = 8
 
 def get_data(filename):
     return np.loadtxt(
@@ -25,10 +29,18 @@ def remap(src, _range):
     dst += _range[0]
     return np.round(dst)
 
+def encode(p, d, hmax = HMAX):
+    res = np.zeros((p.shape[0]))
+    for i in range(res.shape[0]):
+        res[i] = rene.tcode(p[i, :], d, hmax)
+    return res
+
 if __name__ == "__main__":
     feature = get_data(FEATURE_FILE)[:, 1:]
-    label = get_data(LABEL_FILE)[:, 1:]
+    label = get_data(LABEL_FILE)[:, 1:].reshape(-1, 1)
     print(feature.shape, feature)
     print(label.shape, label)
-    feature_remap = remap(feature, (0, 255))
+    feature_remap = remap(feature, (0, 2**W-1))
     print(feature_remap.shape, feature_remap)
+    feature_encode = encode(feature_remap, feature_remap[0].shape[0], HMAX)
+    print(feature_encode)
