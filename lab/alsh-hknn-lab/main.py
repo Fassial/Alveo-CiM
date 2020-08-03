a9a2b4ae80c32e4dca1de25a0e1cd42fe1d3a02d
fassial <2508440340@qq.com>
2020-08-04 01:55:24 +0800

    lab: set n_hashfuncs as 256 in alsh-hknn-lab, prepare to test

--- lab/alsh-hknn-lab/main.py
+++ lab/alsh-hknn-lab/main.py
@@ -11,10 +11,11 @@ from alsh import alsh
 
 # dataset params
 DATASET = os.path.join(".", "dataset")
+SCORE_FILE = os.path.join("scores.csv")
 N_DATASET = 5000
 P_TRAIN = 0.7
 # alsh params
-N_HASHFUNCS = 1024
+N_HASHFUNCS = 256
 K = 10
 
 def main():
@@ -24,8 +25,8 @@ def main():
     # inst alsh
     start_time = timeit.default_timer()
     alsh_inst = alsh(
-        points = x_train[:int(N_DATASET*P_TRAIN)],
-        labels = y_test[:int(N_DATASET*P_TRAIN)],
+        points = x_train,# [:int(N_DATASET*P_TRAIN)],
+        labels = y_test,# [:int(N_DATASET*P_TRAIN)],
         n_hashfuncs = N_HASHFUNCS,
         k = K
     )
@@ -34,11 +35,15 @@ def main():
     # get scores
     start_time = timeit.default_timer()
     scores = alsh_inst.get_score(
-        querys = x_test[:int(N_DATASET*(1-P_TRAIN))],
-        labels = y_test[:int(N_DATASET*(1-P_TRAIN))],
+        querys = x_test,# [:int(N_DATASET*(1-P_TRAIN))],
+        labels = y_test,# [:int(N_DATASET*(1-P_TRAIN))],
         op = "and"
     )
     print(scores)
+    utils.store_data(
+        filename = SCORE_FILE,
+        src = scores
+    )
     end_time = timeit.default_timer()
     print("get score runs for %.1fs" % (end_time - start_time))
 
