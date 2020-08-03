d240481fc425a60a0837b13e1acd5fe25f3e6c61
fassial <2508440340@qq.com>
2020-08-03 18:08:10 +0800

    lab: support buckets in alsh-hknn-lab

--- lab/alsh-hknn-lab/alsh.py
+++ lab/alsh-hknn-lab/alsh.py
@@ -12,18 +12,19 @@ class alsh:
     """
     Adaptive LSH proposed in FoggyCache, MobiCom
     """
+    OP = ["or", "and"]
 
-    def __init__(self, points, a, b, k, _ord = 2):
+    def __init__(self, points, labels, n_hashfuncs, k, _ord = 2):
         """
         Init paramas of alsh.
         Parameters:
         -----------
-        points : :py.class`ndarray <numpy.ndarray>` of shape `(n_point, n_feature)`
+        points : :py.class`ndarray <numpy.ndarray>` of shape `(n_points, n_features)`
             the data set to query
-        a : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfunc, n_feature)`
-            the weights of hashfunc
-        b : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfunc, )`
-            the biases of hashfunc
+        labels : :py.class`ndarray <numpy.ndarray>` of shape `(n_points, )`
+            the labels of the data set to query
+        n_hashfuncs: int
+            the number of hash funcs in the same hash family
         k : int
             the length of neighborhoodList
         _ord : int, default 2
@@ -34,23 +35,29 @@ class alsh:
         """
         # set dataset
         self.points = points
+        self.labels = labels
         # set alsh params
-        self.a = a
-        self.b = b
         self.k = k
         self._ord = _ord
+        # set matrix params
+        self.n_features = self.points.shape[1]
+        self.n_hashfuncs = n_hashfuncs
+        self.a = np.random.rand(self.n_hashfuncs, self.n_features)
+        self.b = np.random.rand(self.n_hashfuncs)
         # get adaptive params
         self.c = self._get_c()
         self.r = self._get_r()
+        # get buckets (<becket_id> -> [idxs of points])
+        self.buckets = self._get_buckets()
 
     def update_ab(self, a, b):
         """
         Update a & b paramas of alsh.
         Parameters:
         -----------
-        a : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfunc, n_feature)`
+        a : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfuncs, n_features)`
             the weights of hashfunc
-        b : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfunc, )`
+        b : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfuncs, )`
             the biases of hashfunc
         Returns:
         --------
@@ -164,52 +171,176 @@ class alsh:
             the adaptive param r of alsh
         """
         # minimize target func
+        # method of minimize
+        # - 'Nelder-Mead' :ref:`(see here) <optimize.minimize-neldermead>`
+        # - 'Powell'      :ref:`(see here) <optimize.minimize-powell>`
+        # - 'CG'          :ref:`(see here) <optimize.minimize-cg>`
+        # - 'BFGS'        :ref:`(see here) <optimize.minimize-bfgs>`
+        # - 'Newton-CG'   :ref:`(see here) <optimize.minimize-newtoncg>`
+        # - 'L-BFGS-B'    :ref:`(see here) <optimize.minimize-lbfgsb>`
+        # - 'TNC'         :ref:`(see here) <optimize.minimize-tnc>`
+        # - 'COBYLA'      :ref:`(see here) <optimize.minimize-cobyla>`
+        # - 'SLSQP'       :ref:`(see here) <optimize.minimize-slsqp>`
+        # - 'trust-constr':ref:`(see here) <optimize.minimize-trustconstr>`
+        # - 'dogleg'      :ref:`(see here) <optimize.minimize-dogleg>`
+        # - 'trust-ncg'   :ref:`(see here) <optimize.minimize-trustncg>`
+        # - 'trust-exact' :ref:`(see here) <optimize.minimize-trustexact>`
+        # - 'trust-krylov':ref:`(see here) <optimize.minimize-trustkrylov>`
+        # - custom - a callable object (added in version 0.14.0)
         res = minimize(
             fun = self._rho_r,
-            x0 = r0,
-            method='SLSQP'
+            x0 = r0
         )
         # get corresponding r
-        r = res.x[0]
+        r = res.x[0]; print("r:", r)
         return r
 
-    def predict(self, query):
+    def forward(self, query):
         """
-        Predict the bucket of query point.
+        Get the bucket(np.array) of query point.
         Parameters:
         -----------
-        query : :py.class`ndarray <numpy.ndarray>` of shape `(n_feature, )`
+        query : :py.class`ndarray <numpy.ndarray>` of shape `(n_features, )`
             query point
         Returns:
         --------
-        bucket : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfunc, )`
+        bucket : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfuncs, )`
             the bucket of query point
         """
         # get h_v
         h_v = np.floor((self.a.dot(query) + self.b) / self.r)
         return h_v.astype(np.int32)
 
+    def _get_key(self, bucket, mask):
+        """
+        Get the key(str) of bucket & mask.
+        Parameters:
+        -----------
+        bucket : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfuncs, )`
+            the encode of bucket
+        mask : :py.class`ndarray <numpy.ndarray>` of shape `(n_hashfuncs, )`
+            the mask of bucket with zeros and ones
+        Returns:
+        --------
+        key : str
+            the key(str) of bucket & mask
+        """
+        key = ""
+        for i in range(bucket.shape[0]):
+            if mask[i] != 0: key += str(bucket[i])
+            else: key += "*"
+            if i != bucket.shape[0]-1: key += "-"
+        return key
+
+    def _get_buckets(self):
+        """
+        Get the buckets of hashtable(temp only one table).
+        Buckets are orginized like:
+            {
+                "0-0": [0],
+                "0-1": [1],
+                "1-0": [2],
+                "1-1": [3],
+                "*-0": [0, 2],
+                "*-1": [1, 3],
+                "0-*": [0, 1],
+                "1-*": [2, 3],
+                "*-*": [0, 1, 2, 3],
+                ...
+            }
+        Remember: key is not limited in [0, 1], N
+        Parameters:
+        -----------
+        None
+        Returns:
+        --------
+        buckets : dict
+            the buckets of hashtable
+        """
+        buckets = dict()
+        for i in range(self.points.shape[0]):
+            # get the original bucket
+            ori_bucket = self.forward(
+                query = self.points[i]
+            )
+            # update buckets
+            # for _and
+            mask = np.ones(ori_bucket.shape)
+            key = self._get_key(ori_bucket, mask)
+            # insert into buckets
+            if buckets.__contains__(key): buckets[key].append(i)
+            else: buckets[key] = [i]
+            # for _or
+            mask = np.zeros(ori_bucket.shape)
+            for j in range(mask.shape[0]):
+                mask[j] = 1
+                key = self._get_key(ori_bucket, mask)
+                # insert into buckets
+                if buckets.__contains__(key): buckets[key].append(i)
+                else: buckets[key] = [i]
+                mask[j] = 0
+        print(buckets.keys())
+        return buckets
+
+    def predict(self, query, op = "or"):
+        """
+        Get the bucket(np.array) of query point.
+        Parameters:
+        -----------
+        query : :py.class`ndarray <numpy.ndarray>` of shape `(n_features, )`
+            query point
+        op : str, {`or`, `and`}
+            query op
+        Returns:
+        --------
+        bucket : list
+            the bucket(list) of query point
+        """
+        bucket = []
+        if op not in alsh.OP: return bucket
+        # get original bucket
+        ori_bucket = self.forward(
+            query = query
+        )
+        # update buckets
+        # for _and
+        mask = np.ones(ori_bucket.shape)
+        key = self._get_key(ori_bucket, mask)
+        # extend bucket
+        if self.buckets.__contains__(key): bucket.extend(self.buckets[key])
+        # end _and
+        if op == "and": return set(bucket)
+        # for _or
+        mask = np.zeros(ori_bucket.shape)
+        for j in range(mask.shape[0]):
+            mask[j] = 1
+            key = self._get_key(ori_bucket, mask)
+            # insert into buckets
+            if self.buckets.__contains__(key): bucket.extend(self.buckets[key])
+            mask[j] = 0
+        if op == "or": return set(bucket)
+        return []
+
 # test params
-N_POINT = 100
-N_FEATURE = 5
-N_HASHFUNC = 4
+N_POINTS = 100
+N_FEATURES = 5
+N_HASHFUNCS = 4
 # alsh params
 K = 10
 
 def test():
     # init points & a & b
-    points = np.array([[i]*N_FEATURE for i in range(N_POINT)])
-    a = np.array([[i*N_FEATURE+j for j in range(N_FEATURE)] for i in range(N_HASHFUNC)])
-    b = np.array([-i for i in range(N_HASHFUNC)])
+    points = np.array([[i]*N_FEATURES for i in range(N_POINTS)])
+    labels = np.array([(i//10) for i in range(N_POINTS)])
     # inst alsh
     alsh_inst = alsh(
         points = points,
-        a = a,
-        b = b,
+        labels = labels,
+        n_hashfuncs = N_HASHFUNCS,
         k = K
     )
     # init query
-    query = np.array([14]*N_FEATURE)
+    query = np.array([100]*N_FEATURES)
     res = alsh_inst.predict(query)
     print(res)
 
---
+++ lab/alsh-hknn-lab/main.py
@@ -0,0 +1,28 @@
+"""
+Created on August 03 18:00, 2020
+
+@author: fassial
+"""
+# local dep
+import utils
+from alsh import alsh
+
+# dataset params
+DATASET = "./predataset"
+# alsh params
+N_HASHFUNCS = 4
+K = 10
+
+def main():
+    x_train, y_train, x_test, y_test = \
+        utils.load_dataset(dirpath = DATASET)
+    # inst alsh
+    alsh_inst = alsh(
+        points = x_train,
+        labels = x_test,
+        n_hashfuncs = N_HASHFUNCS,
+        k = K
+    )
+
+if __name__ == "__main__":
+    main()
